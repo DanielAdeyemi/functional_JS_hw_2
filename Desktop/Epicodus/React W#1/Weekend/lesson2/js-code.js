195ab9870441de7f2a2cb58e2b1ac83a00992911
DanielAdeyemi <adeyemidany@gmail.com>
2021-04-12 21:16:48 -0700

    fix typos and add comments

--- Desktop/Epicodus/React W#1/Weekend/lesson2/js-code.js
+++ Desktop/Epicodus/React W#1/Weekend/lesson2/js-code.js
@@ -1,6 +1,6 @@
-const hydrade = (plant) => {
+const hydrate = (plant) => { //function literal
   return {
     ...plant,
-    water: (plant.water || 0) + 1
+    water: (plant.water || 0) + 1 //return new object representing plant's state
   }
 };
